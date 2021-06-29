881ff784718852dacb76426c55dd4a8d89ebd50c
Evandro <evandro.avellar@gmail.com>
2021-06-29 19:08:10 -0300

    Create regex.sh

---
+++ regex.sh
@@ -0,0 +1,13 @@
+#!/bash/bin
+
+
+cd ./logs
+
+regex "\b([0-9]{1,3}\.){3}[0-9]{1,3}\b"
+
+if [[ $1 =~ $regex ]]
+then
+        cat access.log | grep $1
+else
+        echo "Formato não é válido"
+fi
